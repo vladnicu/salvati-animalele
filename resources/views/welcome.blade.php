233307a9980c078e4fd3eff3a694df9cd58b45ff
Vlad Nicu <nicuvlad89@gmail.com>
2026-02-10 21:55:09 +0200

    added welcome featurette

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,21 +20,33 @@
         </div>
     </div>
 
-    <div class="row mt-4">
-        <div class="col"><img src="{{ Vite::asset('resources/images/Poza homepage 2.jpg') }}" class="img-thumbnail"
-                width="408" height="283" alt="Bine ai venit"></div>
-        <div class="col-8">
-            <h2 class="text-center mb-4">Bine ai venit la SalvatiAnimalele.ro!</h2>
-            <p>Credem că animalele nu sunt invizibile și că fiecare dintre noi poate face o
+    <hr class="featurette-divider" />
+    <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading fw-normal lh-1">
+               Bine ai venit la 
+                <span class="text-body-secondary">SalvatiAnimalele.ro!</span>
+            </h2>
+            <p class="lead">
+                Credem că animalele nu sunt invizibile și că fiecare dintre noi poate face o
                 diferență. Luptăm zilnic pentru a atrage atenția asupra suferinței câinilor fără
                 stăpân, pentru a încuraja adopția și pentru a oferi o a doua șansă celor care
-                au cunoscut doar abandonul și durerea.</p>
-
-            <p>Ne dorim o comunitate mai responsabilă și un viitor în care niciun animal să
-                nu mai fie lăsat în urmă.</p>
-            <button type="button" class="btn btn-primary">Află mai multe despre noi...</button>
+                au cunoscut doar abandonul și durerea.
+            </p>
+            <p class="lead">
+                Ne dorim o comunitate mai responsabilă și un viitor în care niciun animal să
+                nu mai fie lăsat în urmă.
+            </p>
+             <button type="button" class="btn btn-primary">Află mai multe despre noi...</button>
+        </div>
+        <div class="col-md-5 order-md-1">
+            <img src="{{ Vite::asset('resources/images/Poza homepage 2.jpg') }}" class="featurette-image img-fluid mx-auto"
+                width="500" height="500" alt="Bine ai venit">
+     
         </div>
     </div>
+
+    <hr class="featurette-divider" />
 
     <div class="row mt-4 difference-text">
         <div class="col d-flex justify-content-center">
