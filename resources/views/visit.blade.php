a101e718a51b25bc8319ccb2d115a213c639b1ad
Vlad Nicu <nicuvlad89@gmail.com>
2026-02-25 16:19:04 +0200

    added visit content

--- resources/views/visit.blade.php
+++ resources/views/visit.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
     @include('partials.page-title', [
-        'title' => 'Viziteaza adapostul',
+        'title' => 'Despre adăpost și vizitare',
         'subtitle' => 'Adăpostul nostru este deschis vizitatorilor care doresc să cunoască câinii, să petreacă timp
                                     cu ei și să înțeleagă mai bine munca din spatele salvării lor.',
     ])
 
-     @include('partials.hero-image-right', [
-        'title' => 'Adăpostul nostru poate fi vizitat doar cu programare în prealabil',
-        'text' => 'Poți veni singur sau în grup – cu prieteni, colegi, clase de elevi sau ca activitate educațională. Fiecare
-                vizită este o lecție de empatie și grijă față de cei care nu se pot apăra singuri.',
-        'image' => 'Costica4.jpg',
+      @include('partials.hero-image-right', [
+        'title' => 'Adăpostul public pentru câini fără stăpân din Baia Mare',
+        'text' => 'Adapostul funcționează sub autoritatea administrației publice locale, ca parte a serviciului municipal de gestionare a câinilor fără deținător, în conformitate cu legislația națională privind protecția animalelor și gestionarea câinilor fără stăpân, precum și cu reglementările locale aprobate de Consiliul Local Baia Mare.
+         Accesul câinilor în adăpost se realizează exclusiv prin serviciul public de capturare și gestionare a câinilor fără stăpân, prin structurile abilitate ale Poliției Locale Baia Mare, care au atribuția de a prelua și transporta animalele capturate de pe domeniul public. Adăpostul are o capacitate oficială de 419 locuri și asigură cazarea temporară a câinilor în condiții conforme normelor sanitar-veterinare și standardelor de bunăstare animală.',
+        'image' => 'visit2.jpg',
+    ])  
+
+
+    @include('partials.hero-image-center', [
+        'title' => 'Adăpostul public pentru câini fără stăpân din Baia Mare',
+        'text' => 'Adapostul funcționează sub autoritatea administrației publice locale, ca parte a serviciului municipal de gestionare a câinilor fără deținător, în conformitate cu legislația națională privind protecția animalelor și gestionarea câinilor fără stăpân, precum și cu reglementările locale aprobate de Consiliul Local Baia Mare.',
+        'image' => 'visit1.jpg',
     ])
 
+   
     @include('partials.hero-image-left', [
         'title' => 'Pentru câini, aceste vizite înseamnă mai mult decât joacă.',
         'text' => 'În spațiul nostru verde, special amenajat, ei au șansa de a ieși din rutina adăpostului, de a primi atenție
