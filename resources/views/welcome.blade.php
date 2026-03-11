36a656272cbcb3a83750bd618c43f519dff38770
Vlad Nicu <nicuvlad89@gmail.com>
2026-03-11 07:23:48 +0200

    changed homepage photos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="homepage-image-1-row d-flex justify-content-center position-relative d-inline-block">
-        <img src="/images/pozahomepage1.png"  class="img-fluid img-thumbnail" alt="...">
+        <img src="/images/pozahomepage1.png" class="img-fluid img-thumbnail" alt="...">
         <div class="position-absolute top-50 start-50 translate-middle text-white fw-bold">
             {{-- <h1>Salvați Animalele Baia Mare</h1>
             <p>Luptăm zilnic pentru a salva câini abandonați și pentru a le reda
@@ -14,7 +14,7 @@
         {{-- <h2 class="pb-2 border-bottom">Hanging icons</h2> --}}
         <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
 
-            @include('partials.feature', [  
+            @include('partials.feature', [
                 'title' => 'Despre noi',
                 'subtitle' => 'Cine suntem și cum ajutăm',
                 'icon' => 'house-heart',
@@ -42,16 +42,16 @@
     @include('partials.hero-image-left', [
         'title' => 'Bine ai venit la SalvatiAnimalele.ro!',
         'text' => 'Credem că animalele nu sunt invizibile și că fiecare dintre noi poate face o
-                diferență. Luptăm zilnic pentru a atrage atenția asupra suferinței câinilor fără
-                stăpân, pentru a încuraja adopția și pentru a oferi o a doua șansă celor care
-                au cunoscut doar abandonul și durerea.',
+                            diferență. Luptăm zilnic pentru a atrage atenția asupra suferinței câinilor fără
+                            stăpân, pentru a încuraja adopția și pentru a oferi o a doua șansă celor care
+                            au cunoscut doar abandonul și durerea.',
         'image' => 'pozahomepage2.jpg',
     ])
 
     @include('partials.hero-image-center', [
         'title' => 'Află mai multe despre noi...',
         'text' => 'Ne dorim o comunitate mai responsabilă și un viitor în care niciun animal să
-                nu mai fie lăsat în urmă.',
+                            nu mai fie lăsat în urmă.',
         'image' => 'pozahomepage3.png',
         'url' => 'about',
     ])
@@ -60,56 +60,59 @@
 
     <div class="container px-4 py-5" id="hanging-icons">
         <h1 class="pb-2">Cum ne poți ajuta? Împreună putem face diferența</h2>
-        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+            <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
 
-            @include('partials.feature', [
-                'title' => 'Donează',
-                'subtitle' => 'Fiecare donație salvează o viață',
-                'icon' => 'cash',
-                'url' => 'donate',
-            ])
+                @include('partials.feature', [
+                    'title' => 'Donează',
+                    'subtitle' => 'Fiecare donație salvează o viață',
+                    'icon' => 'cash',
+                    'url' => 'donate',
+                ])
 
-            @include('partials.feature', [
-                'title' => 'Salvează',
-                'subtitle' => 'Ajută direct un câine din adăpost',
-                'icon' => 'person-raised-hand',
-                'url' => 'sponsor',
-            ])
+                @include('partials.feature', [
+                    'title' => 'Salvează',
+                    'subtitle' => 'Ajută direct un câine din adăpost',
+                    'icon' => 'person-raised-hand',
+                    'url' => 'sponsor',
+                ])
 
-            @include('partials.feature', [
-                'title' => 'Voluntariat',
-                'subtitle' => 'Dăruiește timp și oferă speranță',
-                'icon' => 'piggy-bank',
-                'url' => '',
-            ])
-        </div>
+                @include('partials.feature', [
+                    'title' => 'Voluntariat',
+                    'subtitle' => 'Dăruiește timp și oferă speranță',
+                    'icon' => 'piggy-bank',
+                    'url' => '',
+                ])
+            </div>
     </div>
 
     <hr class="featurette-divider" />
 
+    <h1 class="pb-2">Le mulțumim companiilor și organizațiilor ne susțin!</h2>
+
     <div class="row mb-4">
-        <div class="col d-flex  justify-content-center"><img src="/images/fam_dogs.jpg"
-                class="img-fluid img-thumbnail" alt="fam dogs" width="100" height="100"></div>
         <div class="col d-flex justify-content-center">
-            <img src="/images/aramis.jpg" class="img-fluid img-thumbnail" alt="aramis"
-                width="100" height="100">
+            <img src="/images/herz.png" class="img-fluid img-thumbnail" alt="herz" width="100" height="100">
         </div>
-        <div class="col d-flex justify-content-center">
-            <img src="/images/herz.png" class="img-fluid img-thumbnail" alt="herz"
-                width="100" height="100">
-        </div>
+
+        <div class="col d-flex justify-content-center"><img src="/images/pet_crew.jpg" class="img-fluid img-thumbnail"
+                alt="pet crew" width="100" height="100"></div>
+
+        <div class="col d-flex  justify-content-center"><img src="/images/fam_dogs.jpg" class="img-fluid img-thumbnail"
+                alt="fam dogs" width="100" height="100"></div>
     </div>
 
     <div class="row">
-        <div class="col d-flex justify-content-center"><img src="/images/pet_crew.jpg"
-                class="img-fluid img-thumbnail" alt="pet crew" width="100" height="100"></div>
         <div class="col d-flex justify-content-center">
-            <img src="/images/pets_rescue.png" class="img-fluid img-thumbnail"
-                alt="pet rescue" width="100" height="100">
+            <img src="/images/pets_rescue.png" class="img-fluid img-thumbnail" alt="pet rescue" width="100"
+                height="100">
         </div>
+
         <div class="col d-flex justify-content-center">
-            <img src="/images/sepultura.png" class="img-fluid img-thumbnail" alt="sepultura"
-                width="100" height="100">
+            <img src="/images/sepultura.png" class="img-fluid img-thumbnail" alt="sepultura" width="100" height="100">
+        </div>
+
+        <div class="col d-flex justify-content-center">
+            <img src="/images/aramis.jpg" class="img-fluid img-thumbnail" alt="aramis" width="100" height="100">
         </div>
     </div>
 @endsection
