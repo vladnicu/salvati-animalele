4713bcb4e5cd062673e160a6dedd804d3ec81e54
Vlad Nicu <nicuvlad89@gmail.com>
2026-03-11 07:24:02 +0200

    change adopt dog-card

--- resources/views/partials/dog-card.blade.php
+++ resources/views/partials/dog-card.blade.php
@@ -2,16 +2,23 @@
      <div class="card shadow-sm">
          <img src="/images/sponsor/{{ $image }}" class="card-img-top" alt="...">
          <div class="card-body">
-             <p class="card-text">
-                 {{$name}}
-             </p>
-             <div class="d-flex justify-content-between align-items-center">
+             <h5 class="card-title">
+                 {{ $name }}
+             </h5>
+             <h6 class="card-subtitle mb-2 text-body-secondary">O surpriză pe patru lăbuțe își caută norocul!</h6>
+             <ul class="list-group list-group-flush">
+                 <li class="list-group-item"> <i class="bi bi-cake"></i> 9 ani</li>
+                 <li class="list-group-item"> <i class="bi bi-gender-male"></i> Mascul</li>
+                 <li class="list-group-item"> <i class="bi bi-rulers"></i> 42cm </li>
+                 <li class="list-group-item"> <i class="bi bi-info-square-fill"></i> Este obișnuit cu copii și cu pisica.</li>
+             </ul>
+             {{-- <div class="d-flex justify-content-between align-items-center">
                  <button type="button" class="btn btn-sm btn-outline-secondary">
                      {{ $buttonText }}
                  </button>
 
                  <small class="text-body-secondary">9 ani</small>
-             </div>
+             </div> --}}
          </div>
      </div>
  </div>
